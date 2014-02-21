7acc6982ef598b70a7d735478df424cc5f9c0234
Ed Shadi <edshadi@gmail.com>
2014-02-20 17:23:44 -0800

    group challenges by phase and unit

--- source/app/models/challenge.rb
+++ source/app/models/challenge.rb
@@ -2,7 +2,7 @@ class Challenge < ActiveRecord::Base
   belongs_to :phase
 
   def self.grouped_by_phase_and_level
-    by_phase = Challenge.all.group_by {|challenge| challenge.phase.name }.sort_by{ |phase, level| level }
+    by_phase = Challenge.all.group_by {|challenge| "#{challenge.phase.name}-#{challenge.level}" }.sort_by{ |phase, level| level }
     Hash[ by_phase ]
   end
 end
--- source/spec/models/challenge_spec.rb
+++ source/spec/models/challenge_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 describe Challenge do
   context ".grouped_by_phase_and_level" do
-    let!(:challenge) { FactoryGirl.create :challenge }
+    let!(:challenge) { FactoryGirl.create :challenge, :level => 1 }
+    let!(:challenge2) { FactoryGirl.create :challenge, :level => 2 }
     it "returns a hash of all challenges grouped by challenge and level" do
-      expect(Challenge.grouped_by_phase_and_level).to eq({challenge.phase.name => [challenge] })
+      results = {
+        "#{challenge.phase.name}-#{challenge.level}" => [challenge],
+        "#{challenge2.phase.name}-#{challenge2.level}" => [challenge2]
+      }
+      expect(Challenge.grouped_by_phase_and_level).to eq(results)
     end
   end
   context "associations" do
