306d0297668eabe837f4c88e4bad37c75ce50f65
Ed Shadi <edshadi@gmail.com>
2014-02-20 16:19:27 -0800

    added source: an empty rails skeleton

---
+++ source/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TabsChallenge::Application.config.secret_token = 'fd18fccf7b88dfeeccdb6d9129878b710f12c8e305083f6c71dd2046b038c9643be58b28ae5aa16dd176ab0f24cb36f96b74ed2035ca7ddc9e2b2086f2e838c6'
