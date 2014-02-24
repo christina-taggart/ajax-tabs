632cfe070e0f3b85db6c3b7a5f97649442773a2a
Ed Shadi <edshadi@gmail.com>
2014-02-23 20:47:42 -0800

    added .image to challenge

--- source/app/models/challenge.rb
+++ source/app/models/challenge.rb
@@ -1,8 +1,19 @@
 class Challenge < ActiveRecord::Base
   belongs_to :unit
 
+  IMAGES = %w{
+    http://cdn.attackofthecute.com/December-13-2013-02-00-13-jj.jpg
+    http://cdn.attackofthecute.com/April-15-2013-00-38-11-jljksdflkjdsf.jpg
+    http://cdn.attackofthecute.com/February-08-2014-02-17-02-c.jpg
+    http://cdn.attackofthecute.com/February-08-2014-04-00-47-sd.jpg
+    http://cdn.attackofthecute.com/January-04-2013-19-39-37-hh.jpg
+  }
   def self.grouped_by_phase_and_level
     by_phase = Challenge.all.group_by {|challenge| "#{challenge.unit.phase.name}-#{challenge.unit.number}" }.sort_by{ |phase, level| level }
     Hash[ by_phase ]
+  end
+
+  def image
+    IMAGES.sample
   end
 end
--- source/spec/models/challenge_spec.rb
+++ source/spec/models/challenge_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 describe Challenge do
+  let(:unit) { FactoryGirl.create :unit }
+  let(:unit2) { FactoryGirl.create :unit }
+  let!(:challenge) { FactoryGirl.create :challenge, :unit => unit}
+  let!(:challenge2) { FactoryGirl.create :challenge, :unit => unit2 }
   context ".grouped_by_phase_and_level" do
-    let(:unit) { FactoryGirl.create :unit }
-    let(:unit2) { FactoryGirl.create :unit }
-    let!(:challenge) { FactoryGirl.create :challenge, :unit => unit}
-    let!(:challenge2) { FactoryGirl.create :challenge, :unit => unit2 }
     it "returns a hash of all challenges grouped by challenge and level" do
       results = {
         "#{challenge.unit.phase.name}-#{challenge.unit.number}" => [challenge],
@@ -12,6 +12,9 @@ describe Challenge do
       }
       expect(Challenge.grouped_by_phase_and_level).to eq(results)
     end
+  end
+  it "#image" do
+    expect(Challenge::IMAGES).to include(challenge.image)
   end
   context "associations" do
     it { should belong_to :unit }
